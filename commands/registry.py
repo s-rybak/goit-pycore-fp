5dd7e7a9bb146bd3f66c7bf16c930d58b45e8392
nata <nata_pustelnyk@ukr.net>
2025-03-26 11:15:23 +0100

    add contact - name, phone, email, birthday

--- commands/registry.py
+++ commands/registry.py
@@ -4,6 +4,8 @@ from commands.test_add_contact import TestAddContactCommand
 from commands.test_get_all_contacts import TestAllContactsCommand
 from repositories.contact_repository import ContactRepository
 from storage.pickle_storage import PickleStorage
+from commands.add_contact import AddContactCommand
+from typing import Optional  # Додайте імпорт
 
 class CommandRegistry:
     def __init__(self):
@@ -12,8 +14,10 @@ class CommandRegistry:
     def register_command(self, command: CommandInterface):
         self.commands[command.call_name] = command
 
-    def get_command(self, name: str) -> CommandInterface | None:
+
+    def get_command(self, name: str) -> Optional[CommandInterface]:  
         return self.commands.get(name)
+
 
     def get_all_commands(self) -> list[CommandInterface]:
         return list(self.commands.values())
@@ -21,8 +25,11 @@ class CommandRegistry:
     def get_command_names(self) -> list[str]:
         return list(self.commands.keys())
     
+    
+    
 
 registry = CommandRegistry()
 registry.register_command(GreetCommand())
 registry.register_command(TestAddContactCommand(ContactRepository(PickleStorage("var/data/contacts.pkl"))))
 registry.register_command(TestAllContactsCommand(ContactRepository(PickleStorage("var/data/contacts.pkl"))))
+registry.register_command(AddContactCommand(ContactRepository(PickleStorage("var/data/contacts.pkl"))))
--- entities/contact.py
+++ entities/contact.py
@@ -1,14 +1,20 @@
 from dataclasses import dataclass
 from datetime import date
+from typing import Optional
 
 @dataclass
 class Contact:
     id: str
     name: str
     phone: str
+    email: Optional[str] = None
+    address: Optional[str] = None
+    birthday: Optional[str] = None  
 
-    #Add a constructor becouse we need to ingonre creation of id
-    def __init__(self, name: str, phone: str):
+    def __init__(self, name: str, phone: str, email: Optional[str] = None, address: Optional[str] = None, birthday: Optional[str] = None):
         self.id = ""
         self.name = name
         self.phone = phone
+        self.email = email
+        self.address = address
+        self.birthday = birthday
