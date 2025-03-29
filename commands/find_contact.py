0e56a0276026a5cb4216b5634181ecde981bb7fb
OlhaOs <olalaadka@gmail.com>
2025-03-29 15:52:58 +0200

    fix find_contact

--- commands/find_contact.py
+++ commands/find_contact.py
@@ -19,16 +19,16 @@ class FindContactCommand(CommandInterface):
         self.contact_repository = contact_repository
 
     def execute(self, input: InputInterface, output: OutputInterface, args: list):
-        output.info(Message("Enter the field to search by (name, phone, email, address, birthday):"))
+        output.info("Enter the field to search by (name, phone, email, address, birthday):")
         field_input = input.input()
-        field = field_input.command
+        field = field_input.text
 
         search_methods = {
             "name": self.contact_repository.findByName,
             "phone": self.contact_repository.findByPhone,
             "email": self.contact_repository.findByEmail,
             "address": self.contact_repository.findByAddress,
-            "birthday": self.contact_repository.findByBirthday,
+            "birthday": self.contact_repository.findByBirthday
         }
 
         if field not in search_methods:
@@ -39,11 +39,11 @@ class FindContactCommand(CommandInterface):
 
         output.info(Message(f"Enter the {field} to search for:"))
         value_input = input.input()
-        value = value_input.command
+        value = value_input.text
 
         results = search_methods[field](value)
         if results:
             output.table(Table(headers=["ID", "Name", "Phone", "Email", "Address", "Birthday"],
                                data=[[c.id, c.name, c.phone, c.email, c.address, c.birthday] for c in results]))
         else:
-            output.display_message(Message("Sorry! No contacts found."))
+            output.display_message(Message("No contacts found."))
